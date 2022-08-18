0bc6fb51bf3a320608ab6b00d5e97cf66e9dd41c
kaeru411r <ike269fr@gmail.com>
2022-08-18 14:16:45 +0900

    勢力の決定、移動ができるように #2

--- Assets/Ikeda/EnemyBase.cs
+++ Assets/Ikeda/EnemyBase.cs
@@ -5,8 +5,17 @@ using UnityEngine;
 
 public class EnemyBase : MonoBehaviour
 {
-    [Tooltip("“G‚Ì‹Z”\")]
+    [Tooltip("‹Z”\")]
     [SerializeField] EnemyType _enemyType;
+    [Tooltip("HP‰Šú’l")]
+    [SerializeField] int _hp = 1;
+
+    /// <summary>¨—Í</summary>
+    EnemyHand _hand = EnemyHand.Enemy;
+
+    /// <summary>¨—Í</summary>
+    public EnemyHand Hand { get => _hand; }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,4 +27,30 @@ public class EnemyBase : MonoBehaviour
     {
         
     }
+
+    /// <summary>
+    /// “n‚µ‚½¨—Í‚ÉˆÚ‚é
+    /// </summary>
+    /// <param name="hand"></param>
+    /// <returns>¨—ÍˆÚ“®‚É¬Œ÷‚µ‚½‚©</returns>
+    public bool JumpSide(EnemyHand hand)
+    {
+        if(hand != _hand)
+        {
+            _hand = hand;
+            return true;
+        }
+        return false;
+    } 
+
+}
+
+/// <summary>
+/// “G‚Ì¨—Í
+/// </summary>
+public enum EnemyHand{
+    /// <summary>“G</summary>
+    Enemy,
+    /// <summary>–¡•û</summary>
+    Player,
 }
