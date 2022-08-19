6fbba785f61ac526ac647fed529a08ed4cc72745
kaeru411r <ike269fr@gmail.com>
2022-08-19 10:23:44 +0900

    梅野さんの進捗アップロード

--- Assets/Umeno/EnemyController.cs
+++ Assets/Umeno/EnemyController.cs
@@ -2,40 +2,72 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : EnemyBase
+[RequireComponent(typeof(CapsuleCollider2D))]
+public class EnemyController: EnemyBase
 {
-    [SerializeField] Vector3 _player;
-    [SerializeField] int _moveSpeed;
+    [SerializeField] Transform _player;
+    //[SerializeField] float _attackArea;
+    //[SerializeField] float _attackIntarval;
     //[SerializeField] EnemyAttack _bullet;
-    [SerializeField] float _attackArea;
-    [SerializeField] float _attackIntarval;
-    [SerializeField] CircleCollider2D _attackRange;
-    float _timer;
+    //[SerializeField] float _patrolArea;
+    //[SerializeField] CircleCollider2D _attackRange;
+    //[SerializeField] Transform _patrol;
+    //[SerializeField] int _moveSpeed;
+    //Transform _patrolPosition;
+    //float _timer;
+    //bool _retrunPatol = true;
+    //private void Start()
+    //{
+    //    _patrolPosition = _patrol;
+    //}
     public override void EnemyUpdate()
     {
-        float dir = Vector2.Distance(_player, transform.position);
-        if (dir <= _attackArea)
+        //Enemyのローカルスケールを変数に代入
+        var EnemyLocalScale = transform.localScale;
+        //プレイヤーがエネミーより左側に行ったらローカルスケールを変更する
+        if(_player.position.x > transform.position.x)
         {
-            _timer += Time.deltaTime;
-            if (_timer >= _attackIntarval)
-            {
-                transform.LookAt(_player);
-                _attackRange.enabled = true;
-            }
-            //Instantiate(_bullet, transform.position, transform.rotation);
-            //transform.Translate(_player - transform.position);
+            EnemyLocalScale.x = -1;
         }
-    }
+        //プレイヤーがエネミーより右側に行ったらローカルスケールを変更する
+        else
+        {
+            EnemyLocalScale.x = 1;
+        }
+        transform.localScale = EnemyLocalScale;
+        //RB.velocity = (_patrolPosition.position).normalized * _moveSpeed;
+        //float dir = Vector2.Distance(_player.position, transform.position);
+        //float ptrolDir = Vector2.Distance(_patrolPosition.position, transform.position);
+        //if(ptrolDir <= _patrolArea)
+        //{
+        //    if(_retrunPatol)
+        //    {
+        //        _playerX = -1;
+        //        _patrolPosition = _patrol;
+        //    }
+        //    else
+        //    {
+        //        _playerX = 1;
+        //        _patrolPosition = _patrol;
+        //    }
+        //}
+        //if (dir <= _attackArea)
+        //{
+        //    _timer += Time.deltaTime;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+        //    if (_timer >= _attackIntarval)
+        //    {
+        //        StartCoroutine(EnemyAttack());
+        //        _timer = 0;
+        //    }
+        //    //Instantiate(_bullet, transform.position, transform.rotation);
+        //    //transform.Translate(_player - transform.position);
+        //}   
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    //IEnumerator EnemyAttack()
+    //{
+    //    _attackRange.enabled = true;
+    //    yield return new WaitForSeconds(0.1f);
+    //    _attackRange.enabled = false;
+    //}
 }
